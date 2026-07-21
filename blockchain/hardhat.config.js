793d32afdd93e055f50770a244869350d5b66acf
Reginald Tesla <reginaldtesla6@gmail.com>
2026-07-21 16:53:31 +0000

    .

--- blockchain/hardhat.config.js
+++ blockchain/hardhat.config.js
@@ -1,7 +1,22 @@
-import "@nomicfoundation/hardhat-ethers";
+// // import "@nomicfoundation/hardhat-ethers";
+// require("@nomicfoundation/hardhat-ethers");
+
+// /** @type import('hardhat/config').HardhatUserConfig */
+// export default {
+//   solidity: "0.8.20",
+//   networks: {
+//     hardhat: {},
+//     localhost: {
+//       url: process.env.BLOCKCHAIN_RPC_URL || "http://127.0.0.1:8545",
+//     },
+//   },
+// };
+
+
+require("@nomicfoundation/hardhat-ethers");
 
 /** @type import('hardhat/config').HardhatUserConfig */
-export default {
+module.exports = {
   solidity: "0.8.20",
   networks: {
     hardhat: {},
